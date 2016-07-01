b57365b6cd3095c28135219d43d5472d30e2a8bd
Carl Henderson <chuckhendo@gmail.com>
2016-07-01 13:55:13 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: "<h1>{{ title }}</h1>"
+})
+export class AppComponent {
+    constructor() {
+        this.title = "Test Title";
+    }
+}
---
+++ scripts/site.js
@@ -0,0 +1,8 @@
+import "babel-polyfill";
+import "zone.js/dist/zone.js";
+
+import {bootstrap} from "@angular/platform-browser-dynamic";
+
+import { AppComponent } from "./app";
+
+bootstrap(AppComponent);
